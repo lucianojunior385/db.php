a28526873a4df174280b0e9a95be46d794ad87f1
lucianojunior385 <lucianojunioroliveira100@gmail.com>
2024-09-20 23:16:57 -0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <h2>Cadastro</h2>
+    <form action="register_process.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Nome" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Senha" required><br>
+        <input type="file" name="profile_pic" accept="image/*" required><br>
+        <button type="submit">Cadastrar</button>
+    </form>
+</body>
+</html>
